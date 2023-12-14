30c120a3070d036700f1049985038c37fda70d40
MadCatMagic <arlocollins52@gmail.com>
2023-12-14 21:58:51 +0000

    improved testing

--- 2023/v2_tests.py
+++ 2023/v2_tests.py
@@ -1,28 +1,73 @@
 from v2 import *
 from traceback import format_exc
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+# by convention, assumes in the formatting that the first value is the one being tested for most assertions, and the second value is the true value
 class TestCase:
 
     class outputElement:
-        def __init__(self, success: bool, failureMessage: str = ""):
+        def __init__(self, success: bool, failureMessage: str = "", raisedError = False):
             self.success = success
+            self.raisedError = raisedError
             self.message = failureMessage
 
         def __str__(self):
             if self.success:
-                return "success"
+                return bcolors.OKGREEN + "." + bcolors.ENDC
+            elif self.raisedError:
+                return bcolors.FAIL + "?" + bcolors.ENDC
             else:
-                return self.message
+                return bcolors.FAIL + "#" + bcolors.ENDC
+            
+    class indentElement:
+        def __init__(self, header = None):
+            self.header = header
+
+        def __str__(self):
+            if self.header != None:
+                return f"{self.header}: "
+            return ""
+        
+    class endIndentation: pass
 
     def __init__(self):
         self.assertions = []
 
-    # each individual call is counted as a 'test', but is within the test it is called from
-    # returns whether the test succeeds, but does not raise an error itself
     def assert_equal(self, a, b, message: str = "") -> bool:
         if message == "":
-            message = f"assertion failed: {a!r} != {b!r}"
+            message = f"assertion failed: '{a!r}' != {b!r}"
+        return self._assert(lambda: a == b, message)
+    
+    def assert_nequal(self, a, b, message: str = "") -> bool:
+        if message == "":
+            message = f"assertion failed: '{a!r}' == {b!r}"
+        return self._assert(lambda: a != b, message)
+    
+    # tests for an absolute or relative error of [tolerance]
+    def assert_equal_approx(self, a, b, tolerance = 1e-8, message: str = "") -> bool:
+        if message == "":
+            message = f"assertion failed: '{a!r}' !~= {b!r}"
+        return self._assert(lambda: abs(a - b) < tolerance or abs((a - b) / b) < tolerance, message)
+        
+    def assert_true(self, v, message: str = "") -> bool:
+        if message == "":
+            message = f"assertion failed: '{v!r}' is not True"
+        return self._assert(lambda: v == True, message)
+    
+    # each individual call is counted as a 'test', but is within the test it is called from
+    def _assert(self, function, message):
         try:
-            assert a == b
+            assert function()
             self.assertions.append(self.outputElement(True))
             return True
         except AssertionError:
@@ -46,18 +91,43 @@ class TestCase:
                 continue
 
             if callable(getattr(self, name)):
+                self.assertions.append(self.indentElement(name[5:]))
                 try:
                     if s != None:
                         s()
-
                     getattr(self, name)()
                 except:
-                    self.assertions.append(f"Error while running test '{name[5:]}':")
-                    self.assertions.append(format_exc())
+                    self.assertions.append(self.outputElement(False, f"Error while running test '{name[5:]}':\n{format_exc()[:-1]}", True))
+                self.assertions.append(self.endIndentation())
 
-    def print(self):
-        for k in self.assertions:
-            print(str(k))
+    def __str__(self):
+        s = ""
+        indents = 1
+        successes = 0
+        failures = 0
+        errors = 0
+        start = -1
+        for i, k in enumerate(self.assertions):
+            if type(k) == self.indentElement:
+                start = i
+                indents += 1
+                if str(k) != "":
+                    s += '  ' * (indents - 1) + str(k)
+            elif type(k) == self.endIndentation:
+                s += "\n"
+                for j in range(start + 1, i):
+                    if not self.assertions[j].success:
+                        s += f"{'  ' * indents}{self.assertions[j].message}\n"
+                indents -= 1
+            else:
+                s += f"{k!s}"
+                if k.success: successes += 1
+                elif k.raisedError: errors += 1
+                else: failures += 1
+        s += f"\nSuccesses: {bcolors.OKGREEN}{successes}{bcolors.ENDC}\n"
+        if failures > 0: s += f"Failures: {bcolors.FAIL}{failures}{bcolors.ENDC}\n"
+        if errors > 0: s += f"Errors: {bcolors.WARNING}{errors}{bcolors.ENDC}\n"
+        return s
 
     
 
@@ -67,24 +137,44 @@ def run_tests():
             print(f"Running tests for '{name}':")
             obj = el()
             obj.RunTests()
-            obj.print()
+            print(obj)
 
-class Vector2Tests(TestCase):
-    def setup(self):
-        self.a = v2(1, 2)
-        self.b = v2(4, 7)
-        self.c = v2(-6, -1)
-        self.d = v2(0.3, -0.4)
+class TestTests(TestCase):
+    def test_expect_to_pass(self):
+        # should all pass
+        self.assert_equal(1, 1)
+        self.assert_equal("abc", "a" + "b" + "c")
+        self.assert_equal([42, "er"], [42, "er"])
+        
+        self.assert_nequal(1, 5)
+        self.assert_nequal(True, False)
+        self.assert_nequal([], None)
+        
+        self.assert_true(True)
+        self.assert_true(4 < 76)
 
-    def test_addition(self):
-        self.assert_equal(self.a + self.b, v2(5, 9))
-        self.assert_equal(self.a + self.c, v2(-5, 1))
-        self.assert_equal(self.a + self.d, v2(1.3, 1.6))
+        self.assert_equal_approx(100000, 100000.0003)
+        self.assert_equal_approx(100000.0003, 100000)
+        self.assert_equal_approx(0, 3e-10)
 
-    def test_subtraction(self):
-        self.assert_equal(self.a - self.b, v2(-3, -5))
-        self.assert_equal(self.a - self.c, v2(7, 3))
-        self.assert_equal(self.a - self.d, v2(0.7, 2.4))
+    def test_expect_to_fail(self):
+        #self.expectFailure(11)
+        # should all fail
+        self.assert_equal(1, 2)
+        self.assert_equal(False, 1)
+        self.assert_equal([], ())
+
+        self.assert_nequal(1, 1.0)
+        self.assert_nequal((), ())
+        self.assert_nequal("abc", "abc")
+
+        self.assert_true(False)
+        self.assert_true("awiodj")
+        
+        self.assert_equal_approx(1000, 1000.001)
+        self.assert_equal_approx(5, 10)
+        self.assert_equal_approx(200, -200)
+
 
 if __name__ == "__main__":
     run_tests()
